92c1643c08a7c4fd4ab922b782a43b90585cbd7d
modkhalid <modkhalid5@gmail.com>
2018-10-14 09:37:33 +0530

    rsort(arr,'flag value') rverse sort use for desending sorting

---
+++ rsort.php
@@ -0,0 +1,10 @@
+<?php
+
+$arr=[4,7,8,9,6,4,45,65,20,30,45,78,"modkhalif"];
+rsort($arr,2);
+print_r($arr);
+
+// 0 and 1 usually used for normal
+// 2 for laxical
+//5 for laxical + ingnore case
+?>
